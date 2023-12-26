a75b31bd74d4a056a580b7b8640bf1b5746057df
KeenanOH <86394469+KeenanOH@users.noreply.github.com>
2023-12-25 20:55:21 -0600

    Commit `AdminView.tsx`

---
+++ src/views/admin/AdminView.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+
+import NavigationBar from "../../components/NavigationBar.tsx"
+import ListRow from "../../components/ListRow.tsx"
+import Button from "../../components/Button.tsx"
+import Footer from "../../components/Footer.tsx"
+import Calendar from "../../components/Calendar.tsx"
+import SchoolsList from "./components/SchoolsList.tsx";
+
+
+export default function AdminView() {
+
+    const [date, setDate] = useState({
+        display: "",
+        value: ""
+    })
+
+    return (
+        <div>
+            <NavigationBar isAdmin={ true } />
+            <div className="flex flex-col items-center pt-8">
+                <div className="flex flex-col py-16">
+                    <Calendar setDate={ setDate } />
+                </div>
+
+                <div className="w-full">
+                    <div className="mx-16 2xl:mx-96">
+                        <p className="text-3xl opacity-50 pb-5">{ date.display }</p>
+                        <div className="space-y-5">
+                            <ListRow>Test</ListRow>
+                            <ListRow>Test</ListRow>
+                            <ListRow>Test</ListRow>
+                        </div>
+                        <div className="flex justify-center md:justify-end pt-4">
+                            <div className="w-48">
+                                <Button onClick={ () => { } }>Add Match</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full pt-32">
+                    <div className="mx-16 2xl:mx-96">
+                        <p className="text-3xl opacity-50 pb-5">Schools</p>
+                        <SchoolsList />
+                        <div className="flex justify-center md:justify-end pt-4">
+                            <div className="w-48">
+                                <Button onClick={ () => { } }>Add School</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full pt-32">
+                    <div className="mx-16 2xl:mx-96">
+                        <p className="text-3xl opacity-50 pb-5">Users</p>
+                        <div className="space-y-5">
+                            <ListRow>KeenanOH</ListRow>
+                            <ListRow>Bob123</ListRow>
+                            <ListRow>JoeDoe</ListRow>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
