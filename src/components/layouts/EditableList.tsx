541b7b184736caccb6151bd3d25eff899a2f0083
KeenanOH <86394469+KeenanOH@users.noreply.github.com>
2024-01-04 11:39:21 -0600

    Center checkbox

--- src/components/layouts/EditableList.tsx
+++ src/components/layouts/EditableList.tsx
@@ -117,17 +117,17 @@ export default function EditableList<T extends Selectable>({ className = "", tit
 
 function EditableListRow({ children, editing, onChange, onClick }: { children: string, editing: boolean, onChange: () => void, onClick?: () => void }) {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center">
             <ConditionalRender bool={ editing }>
                 <input className="mr-8 scale-125 accent-primary" type="checkbox" onChange={ () => { onChange() } } />
             </ConditionalRender>
 
             <div
-                className="flex w-full text-xl py-3 shadow-lg rounded-xl transition ease-in-out hover:scale-105 active:scale-110"
+                className="flex w-full text-xl items-center py-3 shadow-lg rounded-xl transition ease-in-out hover:scale-105 active:scale-110"
                 onClick={ onClick }
             >
-                <span className="ml-6">{children}</span>
-                <img className="ml-auto mr-5" src={ rightWardArrowUrl } alt="link-arrow" />
+                <span className="ml-6">{ children }</span>
+                <img className="ml-auto mr-5 h-4" src={ rightWardArrowUrl } alt="link-arrow" />
             </div>
         </div>
     )
